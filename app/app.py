d3c81bacccb16d6ed6ec81fc4e7d6b19508050e6
Ubuntu <ubuntu@ip-172-31-7-231.ap-south-1.compute.internal>
2026-07-25 10:33:30 +0000

    Restructure project for Jenkins deployment

--- app/app.py
+++ app/app.py
@@ -1,14 +1,74 @@
 from flask import Flask
+import mysql.connector
 
 app = Flask(__name__)
+def get_db_connection():
+    return mysql.connector.connect(
+        host="mysql",
+        user="employee",
+        password="employee123",
+        database="employee_db"
+    )
 
 @app.route("/")
 def home():
-    return """
-    <h1>Employee Management System</h1>
-    <h3>Welcome to TechNova Solutions</h3>
-    <p>Project: Two-Tier Docker & Jenkins Deployment</p>
-    """
+    try:
+        conn = get_db_connection()
+
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * FROM employees")
+
+        employees = cursor.fetchall()
+
+        conn.close()
+
+
+        html_rows = ""
+
+        for employee in employees:
+            html_rows += f"""
+            <tr>
+                <td>{employee[0]}</td>
+                <td>{employee[1]}</td>
+                <td>{employee[2]}</td>
+                <td>₹ {employee[3]}</td>
+            </tr>
+            """
+
+        return f"""
+        <html>
+        <head>
+            <title>Employee Management System</title>
+        </head>
+
+        <body>
+
+            <h1>Employee Management System</h1>
+            <h3>Welcome to TechNova Solutions</h3>
+
+            <table border="1" cellpadding="10" cellspacing="0">
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Department</th>
+                    <th>Salary</th>
+                </tr>
+
+                {html_rows}
+
+            </table>
+
+        </body>
+        </html>
+        """
+
+    except Exception as e:
+        return f"""
+        <h1>Employee Management System</h1>
+        <h3>❌ Database Connection Failed</h3>
+        <p>{e}</p>
+        """
 
 @app.route("/health")
 def health():
